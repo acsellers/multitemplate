6e6ca1ce38cc5962f85b81b0b099ed91bf961c5a
Andrew Sellers <andrew.sellers@gradesfirst.com>
2014-03-29 23:42:24 -0500

    Add integration guide for revel to godoc

--- revel/doc.go
+++ revel/doc.go
@@ -1,7 +1,7 @@
 /*
 multitemplate is a package for executing templates from multiple template languages.
-This package is the revel connector for multitemplate. Below is the details on how to use
-multitemplate in your revel application.
+This package is the revel connector for multitemplate. Below is the details on how to
+setup multitemplate so you can use it in your revel application.
 
 app.conf
   // multitemplate must be added to your module list in your app.conf
@@ -9,23 +9,37 @@ app.conf
   module.jobs=github.com/revel/revel/modules/jobs
 
 app/controllers/init.go
-  // You can set DefaultLayouts for different content types, which are
-  // found in your normal template paths
-  multitemplate.DefaultLayout[multitemplate.HTML] = "layouts/app.html"
 
-  // You must initialize the library in revel's OnAppStart function.
-	revel.OnAppStart(multitemplate.Init)
+  func init() {
+          // You can set DefaultLayouts for different content types, which are
+          // found in your normal template paths
+          multitemplate.DefaultLayout[multitemplate.HTML] = "layouts/app.html"
+
+          // You must initialize the library in revel's OnAppStart function.
+          revel.OnAppStart(multitemplate.Init)
+  }
 
 app/init.go
+
+  // import the multitemplate/revel/app/controllers package and any sub-languages you want
+  // to load
+  import (
+          _ "github.com/acsellers/multitemplate/bham"
+          "github.com/acsellers/multitemplate/revel/app/controllers"
+          "github.com/revel/revel"
+  )
+
+
+
   // If you want to use the auto-refresh functionality in DevMode, you need to add the
   // multitemplate.ReloadFilter to the revel.Filters list.
   func init() {
-    revel.Filters = []revel.Filter{
-      revel.PanicFilter,             // Recover from panics and display an error page instead.
-      revel.RouterFilter,            // Use the routing table to select the right Action
-      multitemplate.ReloadFilter,    // Filter for the multitemplate auto-refresh of templates
-      revel.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
-      ...
+          revel.Filters = []revel.Filter{
+                  revel.PanicFilter,             // Recover from panics and display an error page instead.
+                  revel.RouterFilter,            // Use the routing table to select the right Action
+                  multitemplate.ReloadFilter,    // Filter for the multitemplate auto-refresh of templates
+                  revel.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
+                  ...
 
 app/controllers/base_controller.go
 
@@ -36,8 +50,8 @@ app/controllers/base_controller.go
 
   // Simply replace any *revel.Controllers with multitemplate.Controllers
   type BaseController struct {
-    multitemplate.Controller
-    Txn *database.Transaction
+          multitemplate.Controller
+          Txn *database.Transaction
   }
 
 
