26d890974059e591e8819ca5362475b9977b6dd6
Andrew Sellers <andrew.sellers@gradesfirst.com>
2014-04-04 02:39:18 -0500

    Complete testing matrix for security_test.go

--- security_test.go
+++ security_test.go
@@ -8,7 +8,7 @@ import (
 	. "github.com/acsellers/assert"
 )
 
-func TestSentinel(t *testing.T) {
+func TestSentinelJS(t *testing.T) {
 	Within(t, func(test *Test) {
 		t := New("test_templates")
 		var e error
@@ -31,5 +31,57 @@ func TestSentinel(t *testing.T) {
 		c.Main = "default"
 		c.Blocks["break"] = RenderedBlock{template.HTML(`YES`), JS}
 		test.NoError(t.ExecuteContext(&b, c))
+	})
+}
+
+func TestSentinelHTML(t *testing.T) {
+	Within(t, func(test *Test) {
+		t := New("test_templates")
+		var e error
+		t, e = t.Parse("default", `<html>{{ yield "break" }}</html>`, "default")
+		test.NoError(e)
+
+		c := NewContext(nil)
+		c.Main = "default"
+		c.Blocks["break"] = RenderedBlock{template.HTML(`NONONONO`), HTML}
+		b := bytes.Buffer{}
+		test.NoError(t.ExecuteContext(&b, c))
+		b.Reset()
+
+		c = NewContext(nil)
+		c.Main = "default"
+		c.Blocks["break"] = RenderedBlock{template.HTML(`NONONONO`), CSS}
+		test.IsError(t.ExecuteContext(&b, c))
+
+		c = NewContext(nil)
+		c.Main = "default"
+		c.Blocks["break"] = RenderedBlock{template.HTML(`YES`), JS}
+		test.IsError(t.ExecuteContext(&b, c))
+	})
+}
+
+func TestSentinelCSS(t *testing.T) {
+	Within(t, func(test *Test) {
+		t := New("test_templates")
+		var e error
+		t, e = t.Parse("default", `<html><style>{{ yield "break" }}</style></html>`, "default")
+		test.NoError(e)
+
+		c := NewContext(nil)
+		c.Main = "default"
+		c.Blocks["break"] = RenderedBlock{template.HTML(`NONONONO`), HTML}
+		b := bytes.Buffer{}
+		test.IsError(t.ExecuteContext(&b, c))
+		b.Reset()
+
+		c = NewContext(nil)
+		c.Main = "default"
+		c.Blocks["break"] = RenderedBlock{template.HTML(`NONONONO`), CSS}
+		test.NoError(t.ExecuteContext(&b, c))
+
+		c = NewContext(nil)
+		c.Main = "default"
+		c.Blocks["break"] = RenderedBlock{template.HTML(`YES`), JS}
+		test.IsError(t.ExecuteContext(&b, c))
 	})
 }
