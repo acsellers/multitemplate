cf6841f252a1e108e5158ba9fbd62e23fe170876
Andrew Sellers <andrew.sellers@gradesfirst.com>
2014-04-20 04:17:52 -0500

    Add missing file so terse will work, yay for CI

---
+++ terse/utils.go
@@ -0,0 +1,42 @@
+package terse
+
+import (
+	"fmt"
+	ht "html/template"
+	"strings"
+	"text/template"
+	"text/template/parse"
+)
+
+type resources struct {
+	funcs ht.FuncMap
+	vars  []string
+}
+
+func (rsc *resources) Prelude() string {
+	ps := ""
+	for _, v := range rsc.vars {
+		ps += "{{ " + v + " := $ }}"
+	}
+	return ps
+}
+
+func surround(s string) string {
+	if strings.HasPrefix(strings.TrimSpace(s), "{{") {
+		return s
+	}
+	return "{{ " + s + " }}"
+}
+func actionNode(code string, rsc *resources) (*parse.ActionNode, error) {
+	t := template.New("mule").Funcs(template.FuncMap(rsc.funcs))
+	t, err := t.Parse(rsc.Prelude() + surround(code))
+	if err != nil {
+		return nil, err
+	}
+
+	ln := t.Tree.Root.Nodes[len(t.Tree.Root.Nodes)-1]
+	if an, ok := ln.(*parse.ActionNode); ok {
+		return an, nil
+	}
+	return nil, fmt.Errorf("Node could not be parsed")
+}
