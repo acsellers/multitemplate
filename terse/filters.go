07b98e760ed7ff3c208e13cc4c12127e9e0d6a33
Andrew Sellers <andrew.sellers@gradesfirst.com>
2014-04-25 15:19:30 -0600

    Add documentation for Filters

--- terse/filters.go
+++ terse/filters.go
@@ -1,5 +1,9 @@
 package terse
 
+// Filters is map of available functions that take in the text from the
+// template, then run transformations on it, then return it. The boolean
+// indicates whether the transformed string may have delimited template
+// code within it, so it should run it though a stdlib template parser.
 var Filters = map[string]FilterFunc{
 	"plain": func(s string) (string, bool) {
 		return s, true
